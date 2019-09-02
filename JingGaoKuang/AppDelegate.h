2ab66c27ce7ab3dc2b63d45a839b13a497f04732
chents <1011186201@qq.com>
2019-09-02 11:21:32 +0800

    Initial Commit

---
+++ JingGaoKuang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JingGaoKuang
+//
+//  Created by 陈统帅 on 2019/9/2.
+//  Copyright © 2019 陈统帅. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
